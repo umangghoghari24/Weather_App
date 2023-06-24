d7697d88a2a52085fb831947268adf795bcc8ca7
umangghoghari24 <125755486+umangghoghari24@users.noreply.github.com>
2023-06-24 15:58:42 +0530

    weatherapp

--- lib/getstart.dart
+++ lib/getstart.dart
@@ -31,14 +31,13 @@ class _getstartState extends State<getstart> {
         long = _currentPosition.longitude.toString();
       });
 
-      String url ='https://api.openweathermap.org/data/2.5/forecast?late=$late&long=$long&appid=ff0d0154a0fbf7736676e415048f620b';
-     print(url);
-
+      String url ='https://api.openweathermap.org/data/2.5/weather?lat=$late&lon=$long&appid=ff0d0154a0fbf7736676e415048f620b';
+//     print(url);
       var keys = 'ff0d0154a0fbf7736676e415048f620b';
       var response = await http.get (Uri.parse(url));
       if (response.statusCode == 200) {
         var mydata1 = await jsonDecode(response.body);
-      //  print(response.body);
+       print(response.body);
             Navigator.push(context, MaterialPageRoute(builder: (context) => weather()));
       }
       else {
@@ -54,7 +53,8 @@ class _getstartState extends State<getstart> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          child: Center(
+          child:
+          Center(
             child: ElevatedButton(onPressed: () {
               getLocation();
             },
